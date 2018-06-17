de8f75cd3b78575f8bd611255b1ea8e6222676a5
saik788 <36839219+saik788@users.noreply.github.com>
2018-06-17 11:02:41 -0500

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,27 @@
+
+public class Fibonacci {
+	public static void main(String args[]) {
+	
+	int a=0;
+	int b=1;
+	int counter;
+	int c;
+	System.out.println(a);
+	System.out.println(b);
+	for(counter=1;counter<=100;counter++)
+	{
+		c=a+b;
+		System.out.println(c);
+		a=b;
+		b=c;
+		
+	}	
+	
+	
+	
+	
+	
+	
+	
+	}
+}
