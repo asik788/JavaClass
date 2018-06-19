b992833bc8c09bc5260c8282bf20e013847424ce
saik788 <36839219+saik788@users.noreply.github.com>
2018-06-19 16:44:58 -0500

    Add files via upload

---
+++ MathMethods.java
@@ -0,0 +1,21 @@
+
+public class MathMethods {
+	public static void main(String[] args) {
+
+		int a = (int) Math.abs(-3.2);
+		System.out.println("\n absolute value is:" + a);
+
+		double b = Math.cbrt(8);
+		System.out.println("\n cube root is " + b);
+
+		int c = Math.incrementExact(2);
+		System.out.println("\n value is " + c);
+		
+		double d = Math.log(10);
+		System.out.println("\n logged value is "+d);
+		
+		int e= Math.max(6, 50);
+		System.out.println("\n greater number is "+e);
+	}
+}
+/* These are some of the math methods */
