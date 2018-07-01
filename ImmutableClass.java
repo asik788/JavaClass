22a49825e5ff64f1d4b53ddb36b867842edc5d8b
saik788 <36839219+saik788@users.noreply.github.com>
2018-07-01 12:13:10 -0500

    Add files via upload

---
+++ ImmutableClass.java
@@ -0,0 +1,12 @@
+public final class ImmutableClass{  
+final String pancardNumber;  
+  
+public ImmutableClass(String pancardNumber){  
+this.pancardNumber=pancardNumber;  
+}  
+  
+public String getPancardNumber(){  
+return pancardNumber;  
+}  
+  
+}  
