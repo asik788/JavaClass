0a28d52045806afa5a0cd17553f0b39d4cae48f3
saik788 <36839219+saik788@users.noreply.github.com>
2018-07-02 14:47:39 -0500

    Add files via upload

--- JdbcTest.java
+++ JdbcTest.java
@@ -6,11 +6,12 @@ import java.sql.Statement;
 
 public class JdbcTest {
 	public static void main(String[] args) throws SQLException {
-		Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "ABCabc123");
-		Statement stmt = conn.createStatement();
+		Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "ABCabc123");
+		Statement stmt = con.createStatement();
 		ResultSet rs = stmt.executeQuery("select * from stu");
 		while (rs.next())
-			System.out.println(rs.getInt(1) + "  " + rs.getString(2));
-		conn.close();
+			System.out.println(rs.getInt(1) + "  " + rs.getString(2)+"  " + rs.getInt(3));
+		con.close();
 	}
 }
+	
